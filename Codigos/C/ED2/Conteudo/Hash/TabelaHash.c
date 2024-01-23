c575b5fc79443118a6a1a1e32afc86f2de9d966f
PHGomides <gomidesmoraesp@gmail.com>
2024-01-23 13:33:44 -0300

    hash

---
+++ Codigos/C/ED2/Conteudo/Hash/TabelaHash.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int tabelaHash[10];
+int chave;
+
+int gerarCodigoHash (int chave){
+    return chave % 10;
+}
+
+void inserir(){
+    int posicao = gerarCodigoHash(chave);
+    if(tabelaHash[posicao] != -1){
+        printf("\nLocal da tabela ja ocupado\n");
+		return;
+    }
+    tabelaHash[posicao] = chave;
+}
+
+int main(void){
+    for(int i = 0; i < 10; i++){
+        tabelaHash[i] = -1;
+    }
+
+	int opcao=-1;
+
+	while(opcao!=0){
+		//imprimindo  a tabela
+        printf("\n---------------Tabela---------------\n");
+        for(int i = 0; i < 10; i++){
+            printf("[%d]",tabelaHash[i]);
+        }
+		
+		printf("\n---------------MENU---------------\n");
+		printf("1: Inserir \n");
+		printf("0: SAIR \n");
+		scanf("%d", &opcao);
+
+		switch(opcao){
+
+			case 1:
+                printf("\n------INSERINDO------\n");
+                printf("\n\nDigite a chave: ");
+                scanf("%d",&chave); 
+                inserir();
+			break;
+
+			case 0:
+			    printf("\nTchau! Ate a proxima\n");
+			break;
+
+			default:
+			    printf("\nOpcao inválida! \n");
+			break;
+		}
+	}
+
+    
+
+
+}
